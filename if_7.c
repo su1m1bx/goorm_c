30db4eb0331da483e9e1bb3fa9ac5b3bc329a2e7
su1m1bx <su1m1bx@gmail.com>
2026-03-25 08:15:52 +0900

    선택 7번 정답

---
+++ if_7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > 170 && b > 170 && c > 170) printf("PASS");
+    else if (a <= 170) printf("CRASH %d", a);
+    else if (b <= 170) printf("CRASH %d", b);
+    else if (c <= 170) printf("CRASH %d", c);
+
+    return 0;
+}
