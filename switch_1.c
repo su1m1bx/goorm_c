1275d5641ffa258dd9b0b843caaf199ba14658fb
su1m1bx <su1m1bx@gmail.com>
2026-03-29 23:37:00 +0900

    스위치 1번 정답

---
+++ switch_1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+
+    scanf("%d", &score);
+    score = score /10;
+
+    switch (score) {
+        case 10:
+        printf("A");
+        break;
+        
+        case 9:
+        printf("A");
+        break;
+
+        case 8:
+        printf("B");
+        break;
+
+        case 7:
+        printf("C");
+        break;
+
+        case 6:
+        printf("D");
+        break;
+
+        default:
+        printf("E");
+    }
+    return 0;
+}
